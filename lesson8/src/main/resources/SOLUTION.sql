a9e3365a35cc782e8fe8a65b7e82831df0a058cc
Davron-Adbusalomov <2110117@newuzbekistanuniversity.uz>
2024-03-02 08:33:23 +0500

    8th

--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,1 +1,1 @@
-    SELECT MAX(birthday) from Student; SELECT MIN(payment_date) from Payment; SELECT AVG(m.mark) from Mark m INNER JOIN Subject s on m.subject_id = s.id where s.name = 'Math'; SELECT MIN(amount) from Payment where payment_type_id = (SELECT id from PaymentType where name = 'WEEKLY');
+    SELECT MAX(birthday) from Student; SELECT MIN(payment_date) from Payment; SELECT AVG(m.mark) from Mark m INNER JOIN Subject s on m.subject_id = s.id where s.name = 'Math'; SELECT MIN(amount) from Payment where type_id = (SELECT id from PaymentType where name = 'WEEKLY');
