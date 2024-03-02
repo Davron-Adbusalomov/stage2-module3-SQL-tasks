225f0fd34531ba5c797c2b0feb33cf6e17d1e536
Davron-Adbusalomov <2110117@newuzbekistanuniversity.uz>
2024-03-02 16:29:44 +0500

    13

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-Select * from Payment where amount>=500;SELECT * FROM Student WHERE (YEAR(CURRENT_DATE) - YEAR(birthday)) / 365.25 > 20;SELECT * FROM Student WHERE (YEAR(CURRENT_DATE) - YEAR(birthday)) > 20;SELECT * from Student where name = 'Mike' OR groupnumber IN (4,5,6);SELECT * FROM Payment WHERE payment_date >= CURRENT_DATE() - INTERVAL '8' MONTH AND payment_date <= CURRENT_DATE();SELECT * FROM Student where name like 'A%';SELECT * FROM Student WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9);
+Select * from Payment where amount>=500;SELECT * FROM Student WHERE (YEAR(CURRENT_DATE) - YEAR(birthday)) / 365.25 > 20;SELECT * FROM Student WHERE (YEAR(CURRENT_DATE) - YEAR(birthday)) > 20;SELECT * from Student where name = 'Mike' OR groupnumber IN (4,5,6);SELECT * FROM Payment WHERE payment_date >= CURRENT_DATE() - INTERVAL '8' MONTH AND payment_date <= CURRENT_DATE();SELECT * FROM Student where name like 'A%';SELECT * FROM Student WHERE (name like 'Roxi%' AND groupnumber = 4) OR (name LIKE 'Tallie%' AND groupnumber = 9);
 
 
 
